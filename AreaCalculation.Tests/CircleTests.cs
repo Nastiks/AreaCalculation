710292c8a72baf4b0cff72a710633ad410b53b06
Anastasiia Belova <polumka1804@gmail.com>
2023-01-30 15:56:48 +0600

    Correct test names for the circle

--- AreaCalculation.Tests/CircleTests.cs
+++ AreaCalculation.Tests/CircleTests.cs
@@ -6,14 +6,14 @@ namespace AreaCalculation.Tests
     public class CircleTests
     {
         [Test]
-        public void IsRadius_WitnNull_ThrowsException()
+        public void IsCircle_WithRadiusNull_ThrowsException()
         {
             var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(0));
             Assert.That(exception, Is.Not.Null);
         }
 
         [Test]
-        public void IsRadius_WitnPositiveValue_ReturnSuccess()
+        public void IsCircle_WithRadiusPositiveValue_ReturnSuccess()
         {
             Circle circle = new Circle(5);
             double expected = 28.141592653589793;
@@ -21,7 +21,7 @@ namespace AreaCalculation.Tests
         }
 
         [Test]
-        public void IsRadius_WitnNegativeValue_ThrowsException()
+        public void IsCircle_WithRadiusNegativeValue_ThrowsException()
         {
             var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(-8));
             Assert.That(exception, Is.Not.Null);
