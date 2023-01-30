ae3921d74472feb21efdcf8f6784e700c94cfabd
Anastasiia Belova <polumka1804@gmail.com>
2023-01-30 17:42:40 +0600

    Add a method for determining a right triangle

--- AreaCalculation.Core/Triangle.cs
+++ AreaCalculation.Core/Triangle.cs
@@ -20,10 +20,23 @@
         public double SecondSide { get;}
         public double ThirdSide { get;}
         public double Area => CalculateArea();
+        public bool IsRightTriangle => DetermineRightTriangle();
         private double CalculateArea()
         {
             double semiPerimeter = (FirstSide + SecondSide + ThirdSide) / 2;
             return Math.Sqrt(semiPerimeter * (semiPerimeter - FirstSide) * (semiPerimeter - SecondSide) * (semiPerimeter - ThirdSide));
+        }
+        
+        public bool DetermineRightTriangle()
+        {
+            double maxSide = Math.Max(FirstSide, Math.Max(SecondSide, ThirdSide));
+            double minSide = Math.Min(FirstSide, Math.Min(SecondSide, ThirdSide));
+            double averageSide = (FirstSide + SecondSide + ThirdSide) - maxSide - minSide;
+            if (maxSide == Math.Sqrt((Math.Pow(averageSide, 2)) + (Math.Pow(minSide, 2))))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
